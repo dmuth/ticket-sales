077aa443faf04a5a9570b872fcc88da0be970402
Douglas Muth <dmuth@dmuth.org>
2013-05-24 00:12:29 -0400

    Renamed to "event".

--- test/model/event.js
+++ test/model/event.js
@@ -3,12 +3,12 @@
 var seq = require("seq");
 var log = require("../../lib/util/log.js");
 var should = require("should");
-var venue = require("../../lib/model/venue.js");
+var event = require("../../lib/model/event.js");
 
 //
-// Our venue object
+// Our event object
 //
-var venueObject;
+var eventObject;
 
 
 var data = {
@@ -52,80 +52,80 @@ var data = {
 	};
 
 
-suite("venue", function() {
+suite("event", function() {
 
 
 	/**
-	* Set up and populate our venue object before each test.
+	* Set up and populate our event object before each test.
 	*/
 	setup(function(done) {
-		log.setLevel("debug"); // Debugging
-		venueObject = venue.new(data);
+		//log.setLevel("debug"); // Debugging
+		eventObject = event.new(data);
 		done();
 	});
 
 
 	/**
-	* Destroy our venue object after each test.
+	* Destroy our event object after each test.
 	*/
 	teardown(function(done) {
-		delete venueObject;
+		delete eventObject;
 		done();
 	});
 
 
 	test("test", function(done) {
 
-		var result = venueObject.getNumSeats();
+		var result = eventObject.getNumSeats();
 		result.should.equal(12);
 
-		var result = venueObject.checkSeat("orchestra", "a", "3");
+		var result = eventObject.checkSeat("orchestra", "a", "3");
 		result.should.be.true;
-		var result = venueObject.checkSeat("foobar", "a", "3");
+		var result = eventObject.checkSeat("foobar", "a", "3");
 		result.should.be.false;
 
-		var result = venueObject.lockSeat("orchestra", "a", "3");
+		var result = eventObject.lockSeat("orchestra", "a", "3");
 		result.should.be.true;
-		var result = venueObject.lockSeat("orchestra", "a", "3");
+		var result = eventObject.lockSeat("orchestra", "a", "3");
 		result.should.be.false;
-		var result = venueObject.lockSeat("orchestra", "foobar", "3");
+		var result = eventObject.lockSeat("orchestra", "foobar", "3");
 		result.should.be.false;
-		var result = venueObject.getNumSeats();
+		var result = eventObject.getNumSeats();
 		result.should.equal(11);
 
-		var result = venueObject.unlockSeat("orchestra", "a", "3");
+		var result = eventObject.unlockSeat("orchestra", "a", "3");
 		result.should.be.true;
-		var result = venueObject.unlockSeat("orchestra", "a", "foobar");
+		var result = eventObject.unlockSeat("orchestra", "a", "foobar");
 		result.should.be.false;
-		var result = venueObject.unlockSeat("orchestra", "a", "3");
+		var result = eventObject.unlockSeat("orchestra", "a", "3");
 		result.should.be.false;
-		var result = venueObject.getNumSeats();
+		var result = eventObject.getNumSeats();
 		result.should.equal(12);
 
 		//
 		// Now go through the process of claiming a sold seat
 		//
-		var result = venueObject.lockSeat("orchestra", "a", "7");
+		var result = eventObject.lockSeat("orchestra", "a", "7");
 		result.should.be.true;
-		var result = venueObject.claimSeat("orchestra", "a", "7");
+		var result = eventObject.claimSeat("orchestra", "a", "7");
 		result.should.be.true;
-		var result = venueObject.checkSeat("orchestra", "a", "7");
+		var result = eventObject.checkSeat("orchestra", "a", "7");
 		result.should.be.false;
-		var result = venueObject.getNumSeats();
+		var result = eventObject.getNumSeats();
 		result.should.equal(11);
 
 		//
 		// We're going to test unlockSeats(), which will be called 
 		// periodically when running as a webserver.
 		//
-		var result = venueObject.lockSeat("orchestra", "b", "3");
+		var result = eventObject.lockSeat("orchestra", "b", "3");
 		result.should.be.true;
-		var result = venueObject.lockSeat("orchestra", "b", "7");
+		var result = eventObject.lockSeat("orchestra", "b", "7");
 		result.should.be.true;
-		var result = venueObject.getNumSeats();
+		var result = eventObject.getNumSeats();
 		result.should.equal(9);
-		venueObject.unlockSeats();
-		var result = venueObject.getNumSeats();
+		eventObject.unlockSeats();
+		var result = eventObject.getNumSeats();
 		result.should.equal(11);
 
 		done();
